f2678c8707c61619e2bef2156388e1a3f4233891
Guilherme Santos <94789488+Guifecha@users.noreply.github.com>
2024-04-20 00:07:26 +0100

    update trees

--- main.js
+++ main.js
@@ -10,8 +10,6 @@ let targetRotation = 90
 let isTurningBack = false;
 let gameOver = false;
 
-
-
 document.addEventListener('keydown', function (event) {
     keys[event.code] = true;
 }, false);
@@ -43,15 +41,15 @@ function createRenderer() {
 }
 
 function createGround(scene) {
-    var groundTexture = new THREE.TextureLoader().load( 'imports/textures/floor.jpg' );
+    var groundTexture = new THREE.TextureLoader().load( 'imports/textures/floor2.jpg' );
     groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
     groundTexture.repeat.set( 100, 100 );
     groundTexture.anisotropy = 16;
     groundTexture.encoding = THREE.sRGBEncoding;
 
-    var groundMaterial = new THREE.MeshPhongMaterial( { map: groundTexture } );
+    var groundMaterial = new THREE.MeshPhongMaterial({color: 0xffffff, map: groundTexture});
 
-    var groundGeometry = new THREE.PlaneGeometry( 15000, 15000 );
+    var groundGeometry = new THREE.PlaneGeometry( 30000, 30000 );
 
     var ground = new THREE.Mesh( groundGeometry, groundMaterial );
     ground.receiveShadow = true; // Set the ground to receive shadows
@@ -109,10 +107,54 @@ function createSkybox(scene) {
 
     for (let i = 0; i < 6; i++)
         materialArray[i].side = THREE.BackSide;
-    let skyboxGeo = new THREE.BoxGeometry( 15000, 10000, 15000);
+    let skyboxGeo = new THREE.BoxGeometry( 30000, 15000, 30000);
     let skybox = new THREE.Mesh( skyboxGeo, materialArray );
-    skybox.position.set(0,0,-6000)
+    skybox.position.set(0,5000,-6000)
     scene.add( skybox );  
+}
+
+function createTree(cylinderHeight, cylinderRadius, coneHeight, baseConeRadius, positionx, positionz) {
+    const cylinderGeometry = new THREE.CylinderGeometry(cylinderRadius, cylinderRadius, cylinderHeight, 32);
+    const redMaterial = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+    const cylinder = new THREE.Mesh(cylinderGeometry, redMaterial);
+    cylinder.castShadow = true;
+    cylinder.receiveShadow = true;
+    cylinder.position.y = cylinderHeight / 2.0;
+    const coneGeometry = new THREE.ConeGeometry(baseConeRadius, coneHeight, 32);
+    const greenMaterial = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+    const cone = new THREE.Mesh(coneGeometry, greenMaterial);
+    cone.castShadow = true;
+    cone.receiveShadow = true;
+    cone.position.y = cylinderHeight + coneHeight / 2.0;
+    const tree = new THREE.Group();
+    tree.add(cylinder);
+    tree.add(cone);
+    tree.position.x = positionx;
+    tree.position.z = positionz;
+
+    return tree;
+}
+
+const forest = new THREE.Group();
+function createForest(scene, xpos, zpos) {
+    
+    const tree1 = createTree(100, 100, 300, 400, xpos-100, zpos-100);
+    forest.add(tree1);
+    const tree2 = createTree(100, 100, 200, 20, xpos+100, zpos+100);
+    forest.add(tree2);
+    const tree3 = createTree(100, 10, 200, 20, xpos+300, zpos+300);
+    forest.add(tree3);
+    const tree4 = createTree(100, 10, 200, 20, xpos-300, zpos-300);
+    forest.add(tree4);
+    const tree5 = createTree(100, 100, 300, 400, xpos-500, zpos-700);
+    forest.add(tree5);
+    const tree6 = createTree(100, 100, 200, 20, xpos+900, zpos+300);
+    forest.add(tree6);
+    const tree7 = createTree(100, 10, 200, 20, xpos+1200, zpos-1200);
+    forest.add(tree7);
+    const tree8 = createTree(100, 10, 200, 20, xpos-500, zpos+300);
+    forest.add(tree8);
+    scene.add(forest);
 }
 
 function createControls(camera, domElement) {
@@ -123,14 +165,11 @@ function createControls(camera, domElement) {
     }, false);
 }
 
-
-
 function loadModel(scene, camera, renderer) {
     const loader = new FBXLoader();
     loader.load('imports/models/Character_Soldier.fbx', function (object) {
         model = object;
-        model.position.set(0, 0, -8000);
-        controls.getObject().position.z = -8000;
+        model.position.set(0, 0, -12000);
 
         // Set the camera's position relative to the model
         camera.position.set(0, 170, 0); // Adjust the y value to match the model's height
@@ -141,9 +180,6 @@ function loadModel(scene, camera, renderer) {
         
     });
 }
-
-
-
 
 function loadModel2() {
     const loader = new FBXLoader();
@@ -160,8 +196,6 @@ function loadModel2() {
         toggleLight();
     });
 }
-
-
 
 function toggleLight() {
     if (!model2) return;
@@ -182,10 +216,7 @@ function toggleLight() {
     setTimeout(toggleLight, time);
 }
 
-
-
-
-document.addEventListener('keydown', function(event) {
+/*document.addEventListener('keydown', function(event) {
     const key = event.key;
     if (isRedLight && document.pointerLockElement &&(key === "w" || key === "a" || key === "s" || key === "d")) {
         showLoseScreen();
@@ -196,7 +227,7 @@ document.addEventListener('mousemove', function(event) {
     if (isRedLight && document.pointerLockElement) {
         showLoseScreen();
     }
-}, false);
+}, false);*/
 
 function showLoseScreen() {
     if (gameOver) return;
@@ -224,10 +255,22 @@ function showWinScreen() {
     });
 }
 
+function checkCollision(model, forest) {
+    for (let i = 0; i < forest.length; i++) {
+        let tree = forest[i];
+        let distance = model.position.distanceTo(tree.position);
+
+        // Assuming size is the diameter of the model and tree
+        if (distance < (model.size / 2 + tree.size / 2)) {
+            return true; // Collision detected
+        }
+    }
+    return false; // No collision
+}
 
 
 function animate(renderer, scene, camera) {
-    const speed = 10; // Adjust the speed of movement
+    const speed = 100; // Adjust the speed of movement
     let velocityY = 0;
 
     // Calculate the forward and right vectors of the camera
@@ -239,52 +282,52 @@ function animate(renderer, scene, camera) {
     right.crossVectors(forward, new THREE.Vector3(0, 1, 0));
     console.log(controls.getObject().position.z)
 
+    if (gameOver) return;
     
 
     if (keys['KeyW']) {
         model.position.addScaledVector(forward, speed);
-        controls.getObject().position.addScaledVector(forward, speed);
     }
     if (keys['KeyS']) {
         model.position.addScaledVector(forward, -speed);
-        controls.getObject().position.addScaledVector(forward, -speed);
     }
     if (keys['KeyA']) {
         model.position.addScaledVector(right, -speed);
-        controls.getObject().position.addScaledVector(right, -speed);
     }
     if (keys['KeyD']) {
         model.position.addScaledVector(right, speed);
-        controls.getObject().position.addScaledVector(right, speed);
     }
     if (keys['Space'] && !isJumping) {
         isJumping = true;
-        velocityY = 100; // Initial upward velocity
+        velocityY = 100;
     }
 
     if (model2 && Math.abs(model2.rotation.y - targetRotation) > 0.01) {
-        model2.rotation.y += (targetRotation - model2.rotation.y) * 0.03; // Adjust the speed of rotation
+        model2.rotation.y += (targetRotation - model2.rotation.y) * 0.03; 
     } else if (isTurningBack) {
         isTurningBack = false;
     }
+    
 
-    if (controls.getObject().position.z < -1000) { // Adjust this value to match the z-coordinate of the line
+    if (model.position.z > -1000) { 
         showWinScreen();
         
     }
 
+    if (checkCollision(model, forest)) {
+        console.log('Collision detected!');
+    }
+
     if (isJumping) {
-        velocityY -= 3; // Gravity
+        velocityY -= 3;
         model.position.y += velocityY;
 
-        // If model has landed
         if (model.position.y <= 0) {
             model.position.y = 0;
             velocityY = 0;
             isJumping = false;
         }   
     }
-    
     
     renderer.render(scene, camera);
     requestAnimationFrame(() => animate(renderer, scene, camera));
@@ -298,6 +341,10 @@ const renderer = createRenderer();
 addlight(scene);
 createGround(scene);
 createSkybox(scene);
+createForest(scene, -500, -5000);
+createForest(scene, 43, -8000);
+createForest(scene, 305, -2000);
+createForest(scene, 190, -6000);
 createFinishLine(scene);
 createControls(camera, renderer.domElement);
 toggleLight();
