c586a3d5695b61a4d157a6d7d31b63de4272d779
Guilherme Santos <guifecha@gmail.com>
2024-04-19 16:12:50 +0100

    updates

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ import * as THREE from './imports/js/three.module.js';
 import { PointerLockControls } from './imports/js/PointerLockControls.js';
 import { FBXLoader } from './imports/js/FBXLoader.js';
 
-let model, controls;
+let model,model2, controls;
 let isJumping = false;
 let keys = {};
+let isRedLight = true;
+let targetRotation = 90 
+let isTurningBack = false;
+let gameOver = false;
 
 
 
@@ -47,12 +51,12 @@ function createGround(scene) {
 
     var groundMaterial = new THREE.MeshPhongMaterial( { map: groundTexture } );
 
-    var groundGeometry = new THREE.PlaneGeometry( 10000, 10000 );
+    var groundGeometry = new THREE.PlaneGeometry( 15000, 15000 );
 
     var ground = new THREE.Mesh( groundGeometry, groundMaterial );
     ground.receiveShadow = true; // Set the ground to receive shadows
     ground.rotation.x = - Math.PI / 2; // rotate it to lie flat
-    ground.position.set(0,0,0)
+    ground.position.set(0,0,-6000)
 
     scene.add( ground );
 }
@@ -62,7 +66,7 @@ function addlight(scene) {
     scene.add(light);
 
     const light2 = new THREE.DirectionalLight(0xffffff, 2);
-    light2.position.set(5000, 8000, 5000);
+    light2.position.set(5000, 8000, -5000);
     light2.castShadow = true;
 
     // Create an Object3D to serve as the target for the light
@@ -74,6 +78,17 @@ function addlight(scene) {
     light2.target = targetObject; // set the target of the light
 
     scene.add(light2);
+}
+
+function createFinishLine(scene) {
+    const finishLineTexture = new THREE.TextureLoader().load('imports/textures/finish.png'); // Replace with the path to your finish line texture
+    const finishLineMaterial = new THREE.MeshBasicMaterial({ map: finishLineTexture });
+    const finishLineGeometry = new THREE.BoxGeometry(15000, 10, 300); // Adjust these values to change the size of the finish line
+
+    const finishLine = new THREE.Mesh(finishLineGeometry, finishLineMaterial);
+    finishLine.position.set(0, 0, -1000); // Adjust these values to change the position of the finish line
+
+    scene.add(finishLine);
 }
 
 function createSkybox(scene) {
@@ -94,8 +109,9 @@ function createSkybox(scene) {
 
     for (let i = 0; i < 6; i++)
         materialArray[i].side = THREE.BackSide;
-    let skyboxGeo = new THREE.BoxGeometry( 10000, 10000, 10000);
+    let skyboxGeo = new THREE.BoxGeometry( 15000, 10000, 15000);
     let skybox = new THREE.Mesh( skyboxGeo, materialArray );
+    skybox.position.set(0,0,-6000)
     scene.add( skybox );  
 }
 
@@ -113,7 +129,8 @@ function loadModel(scene, camera, renderer) {
     const loader = new FBXLoader();
     loader.load('imports/models/Character_Soldier.fbx', function (object) {
         model = object;
-        model.position.set(-900, 0, -900);
+        model.position.set(0, 0, -8000);
+        controls.getObject().position.z = -8000;
 
         // Set the camera's position relative to the model
         camera.position.set(0, 170, 0); // Adjust the y value to match the model's height
@@ -128,8 +145,89 @@ function loadModel(scene, camera, renderer) {
 
 
 
+function loadModel2() {
+    const loader = new FBXLoader();
+    loader.load('imports/models/charpolice.fbx', function (object) {
+        model2 = object;
+        model2.position.set(0, 0, -1000);
+
+        const startingRotation = 90 * (Math.PI / 180); // Adjust this value to set the starting rotation (in radians)
+        model2.rotation.y = startingRotation;
+
+        const scaleFactor = 5; // Adjust this value to scale the model
+        model2.scale.set(scaleFactor, scaleFactor, scaleFactor);
+        scene.add(model2);
+        toggleLight();
+    });
+}
+
+
+
+function toggleLight() {
+    if (!model2) return;
+
+    if (isRedLight) {
+        isRedLight = false;
+        isTurningBack = true;
+        targetRotation = 2 * Math.PI; 
+    } else {
+        targetRotation = Math.PI;
+        // Add a delay before setting isRedLight to true
+        setTimeout(() => {
+            isRedLight = true;
+        }, 500); // Adjust this value to change the delay
+    }
+
+    let time = isRedLight ? 3000 : Math.random() * (10000 - 2000) + 2000; 
+    setTimeout(toggleLight, time);
+}
+
+
+
+
+document.addEventListener('keydown', function(event) {
+    const key = event.key;
+    if (isRedLight && document.pointerLockElement &&(key === "w" || key === "a" || key === "s" || key === "d")) {
+        showLoseScreen();
+    }
+}, false);
+
+document.addEventListener('mousemove', function(event) {
+    if (isRedLight && document.pointerLockElement) {
+        showLoseScreen();
+    }
+}, false);
+
+function showLoseScreen() {
+    if (gameOver) return;
+    gameOver = true;
+    const messageDiv = document.getElementById('Screen');
+    console.log("You lose!")
+    messageDiv.textContent = "You lose!";
+    messageDiv.style.display = "block";
+    const rematchButton = document.getElementById('rematchButton');
+    rematchButton.addEventListener('click', function() {
+        window.location.reload();
+    });
+}
+
+function showWinScreen() {
+    if (gameOver) return;
+    gameOver = true;
+    console.log("You win!")
+    const messageDiv = document.getElementById('Screen');
+    messageDiv.textContent = "You win!";
+    messageDiv.style.display = "block";
+    const rematchButton = document.getElementById('rematchButton');
+    rematchButton.addEventListener('click', function() {
+        window.location.reload();
+    });
+}
+
+
+
 function animate(renderer, scene, camera) {
-    const speed = 100; // Adjust the speed of movement
+    const speed = 10; // Adjust the speed of movement
     let velocityY = 0;
 
     // Calculate the forward and right vectors of the camera
@@ -139,25 +237,42 @@ function animate(renderer, scene, camera) {
     forward.normalize();
     const right = new THREE.Vector3();
     right.crossVectors(forward, new THREE.Vector3(0, 1, 0));
+    console.log(controls.getObject().position.z)
 
     
 
     if (keys['KeyW']) {
         model.position.addScaledVector(forward, speed);
+        controls.getObject().position.addScaledVector(forward, speed);
     }
     if (keys['KeyS']) {
         model.position.addScaledVector(forward, -speed);
+        controls.getObject().position.addScaledVector(forward, -speed);
     }
     if (keys['KeyA']) {
         model.position.addScaledVector(right, -speed);
+        controls.getObject().position.addScaledVector(right, -speed);
     }
     if (keys['KeyD']) {
         model.position.addScaledVector(right, speed);
+        controls.getObject().position.addScaledVector(right, speed);
     }
     if (keys['Space'] && !isJumping) {
         isJumping = true;
         velocityY = 100; // Initial upward velocity
     }
+
+    if (model2 && Math.abs(model2.rotation.y - targetRotation) > 0.01) {
+        model2.rotation.y += (targetRotation - model2.rotation.y) * 0.03; // Adjust the speed of rotation
+    } else if (isTurningBack) {
+        isTurningBack = false;
+    }
+
+    if (controls.getObject().position.z < -1000) { // Adjust this value to match the z-coordinate of the line
+        showWinScreen();
+        
+    }
+
     if (isJumping) {
         velocityY -= 3; // Gravity
         model.position.y += velocityY;
@@ -170,11 +285,11 @@ function animate(renderer, scene, camera) {
         }   
     }
     
-
-
+    
     renderer.render(scene, camera);
     requestAnimationFrame(() => animate(renderer, scene, camera));
 }
+
 
 // Execution
 const scene = createScene();
@@ -183,5 +298,9 @@ const renderer = createRenderer();
 addlight(scene);
 createGround(scene);
 createSkybox(scene);
+createFinishLine(scene);
 createControls(camera, renderer.domElement);
+toggleLight();
+loadModel2();
 loadModel(scene, camera, renderer);
+
